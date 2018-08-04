4202050de3e7324cbc301fe49a80801eff0d0bba
Coemgen <coemgen@hotmail.com>
2018-08-03 21:01:51 -0400

    WIP
    replacing prior code with rewritten code.

--- globals.js
+++ globals.js
@@ -1,36 +1,20 @@
-"use strict";
+const ORDERS_ARR = [
+    [
+        "PAPA GINO'S", // restaurant (key 1)
+        1532469292249, // filed time (key 2)
+        1533117500000, // order by time in milliseconds
+        1533117600000, // pickup time in milliseconds
+        "Smith,John", // userID
+        "pizza", // order text
+        9.99 // price
+    ],
+    ["BERTUCCI'S",1532469292000,1533117300000,1533117400000,"Smith,Lisa","giambotta",18.99],
+    ["PAPA GINO'S",1532469292249,,,"Griffin,Kevin","Large pizza",11.99]
+];
 
-const userID = "Griffin,Kevin";
+const USER_ID = "Griffin,Kevin";
 
-/**
- * orderPickupDate
- * restaurant
- * orderByDate     // order coordinator only
- * numberOfOrders  // order coordinator only
- * userID
- * orderText
- * orderPrice
- */
-const ordersArr = [
-    ["2018-07-21T14:15", "BERTUCCI'S", "2018-07-19T14:30", "20",
-        "Griffin,Kevin", "large pizza", "18.99"
-    ],
-    ["2018-07-21T14:15", "BERTUCCI'S", "", "",
-        "Jones,Sal", "small pizza", "12.99"
-    ],
-    ["2018-07-21T14:15", "BERTUCCI'S", "", "",
-        "Smith,Amy", "salad", "8.99"
-    ],
-    ["2018-07-21T14:15", "BERTUCCI'S", "", "",
-        "Brown,John", "spaghetti", "9.99"
-    ],
-    ["2018-07-22T15:00", "PAPA GINOS", "2018-07-19T14:45", "2",
-        "Steel,Lisa", "large pizza", "10.99"
-    ],
-    ["2018-07-22T15:00", "PAPA GINOS", "", "",
-        "Regan,Tom", "cheese pizza", "8.99"
-    ],
-    ["2018-07-21T14:15", "BERTUCCI'S", "", "",
-        "Plant,Sally", "salmon", "16.99"
-    ]
+const REST_ARR = [
+    ["BERTUCCI'S", "https://www.bertuccis.com"],
+    ["PAPA GINO'S", "https://www.papaginos.com"]
 ];
--- javascript.js
+++ javascript.js
@@ -1,377 +1,219 @@
 /*jshint browser:true, jquery:true*/
-/*globals HT, ordersArr*/
+/*global ORDERS_ARR, REST_ARR*/
 
 /**
- * @file Hog Trough
- * @version 0.0
- * @author Kevin Griffin <kevin.griffin@gmail.com>
- * @todo Write the documentation.
- * @todo Implement this function.
- */
-
-/**
- * Hog Trough
- * <p>This is an anonymous closure which is the fundamental construct that makes
- * it all possible, and really is the single best feature of JavaScript. We’ll
- * simply create an anonymous function, and execute it immediately. All of the
- * code that runs inside the function lives in a closure, which provides
- * privacy and state throughout the lifetime of our application.
- * See [Module Pattern]{@link
- * http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html}.</p>
- * @namespace HT
- */
-(function () {
+* Utils
+*/
+const Utils = (function () {
     "use strict";
-    window.HT = {};
-}());
-
-/**
- * @namespace Utils
- * @memberof HT
- */
-(function () {
-    "use strict";
-    HT.Utils = (function () {
-        /**
-         * Format decimal number into a USD money string.
-         * @function get
-         * @memberof! HT.Restaurant
-         * @param {number} cashNum
-         * @returns {object} Restaurants object.
-         */
-        let monify = function (cashNum) {
-            return cashNum.toLocaleString(
-                "en-US", {
-                    "style": "currency",
-                    "currency": "USD"
-                });
-        };
-        return {
-            monify
-        };
-
-    }());
-}());
-
-/**
- * @namespace Restaurant
- * @memberof HT
- */
-(function () {
-    "use strict";
-    HT.Restaurant = (function () {
-        let json = {
-            "--TBD--": "http://www.google.com",
-            "BERTUCCI'S": "http://www.bertuccis.com/",
-            "PAPA GINOS": "https://www.papaginos.com/menu"
-        };
-        /**
-         * Get Restaurants object.
-         * @function get
-         * @memberof! HT.Restaurant
-         * @returns {object} Restaurants object.
-         */
-        let get = function () {
-            return json;
-        };
-        return {
-            get
-        };
-    }());
-}());
-
-/**
- * @namespace UtcDate
- * @memberof HT
- */
-(function () {
-    "use strict";
-    HT.UtcDate = (function () {
-        let d = new Date();
-        let yyyy = d.getFullYear();
-        let mm = d.getMonth() + 1;
-        let dd = d.getDate();
-        let hh = d.getHours();
-        let MM = d.getMinutes();
-        let utcDate = yyyy + "-" +
-            mm.toString().padStart(2, "0") + "-" +
-            dd.toString().padStart(2, "0") +
-            "T" +
-            hh.toString().padStart(2, "0") + ":" +
-            MM.toString().padStart(2, "0") + ":" +
-            "00";
-        return {
-            /**
-             * Get current date in UTC format.
-             * @function get
-             * @memberof! HT.UtcDate
-             * @returns {string} The current date in UTC format.
-             */
-            get: function () {
-                return utcDate;
+    const datify = function (milSecs) {
+        let dt = new Date(milSecs);
+        return dt.toLocaleString("en-US");
+    };
+    const monify = function (sum) {
+        return sum.toLocaleString(
+            "en-US",
+            {
+                style: "currency",
+                currency: "USD",
+                maximumFractionDigits: 2
             }
-        };
-    }());
+        );
+    };
+    return {
+        datify,
+        monify
+    };
 }());
 
 /**
- * @namespace GroupOrder
- * @memberof HT
- */
-(function () {
+*
+*/
+const Restaurant = (function () {
     "use strict";
-    HT.GroupOrder = (function ($) {
-        return {
-            /**
-             * Open new Group Order page.
-             * @function new
-             * @memberof! HT.GroupOrder
-             * @param {object} jQuery The jQuery global.
-             */
-            new: function () {
-                // $("div#main").attr("hidden", true);
-                $("div#main").empty();
-                $("#ordersview").attr("hidden", true);
-                $("#newgrouporder").attr("hidden", false);
-                // restaurant SELECT should be in restaurant module???
-                Object.entries(HT.Restaurant.get()).forEach(
-                    function (curVal) {
-                        $("select#restaurant").append(
-                            "<option value=\"" + curVal[0] + "\">" +
-                            curVal[0] + "</option>"
-                        );
-                    }
-                );
-                $("input#orderByTime").val(HT.UtcDate.get());
-                $("input#orderPickupTime").val(HT.UtcDate.get());
-            }
-        };
-    }(jQuery));
-}());
-
-/**
- * @namespace OneOrder
- * @memberof HT
- */
-(function () {
-    "use strict";
-    HT.OneOrder = (function ($) {
-        return {
-            /**
-             * Open new Group Order page.
-             * @function new
-             * @memberof! HT.GroupOrder
-             * @param {string} restaurant
-             */
-            new: function (restaurant) {
-                $("div#main").empty();
-                $("#ordersview").attr("hidden", true);
-                $("#newgrouporder").attr("hidden", false);
-                Object.entries(HT.Restaurant.get()).forEach(
-                    function (curVal) {
-                        $("select#restaurant").append(
-                            "<option value=\"" + curVal[0] + "\"" +
-                            (restaurant === curVal[0] ? " selected>" : ">") +
-                            curVal[0] + "</option>"
-                        );
-                    }
-                );
-                $("select#restaurant").attr("disabled", true);
-                $("select#restaurant option:selected")
-                    .trigger("change");
-                $("input#orderByTime").val(HT.UtcDate.get());
-                $("input#orderPickupTime").val(HT.UtcDate.get());
-            }
-        };
-    }(jQuery));
-}());
-
-/**
- * @namespace Orders
- * @memberof HT
- */
-(function () {
-    "use strict";
-    let tableHtml = "";
-    let curDate = HT.UtcDate.get();
-    let getObj = function (ordersArr) {
-        /*
-        forEach order
-            if pickup time is less than current plus a couple of hours
-                if restaurant not indexed for this time
-                    add restaurant
-                        add order
-                else add order to the proper restaurant
-        {
-            PAPA GINOS: {
-                pickuptime:2018...,
-                orderbytime:2018...,
-                numoforders:20,
-                orders: [{placer:"Griffin,Kevin",order:Pizza,price:12.00},...]
+    const json = (function () {
+        return REST_ARR.reduce(
+            function (json, restArr) {
+                json[restArr[0]] = restArr[1];
+                return json;
             },
-            ....
-        },
-        */
-        return ordersArr.reduce(
-            function (obj, curVal) {
-                /*
-                 * curVal =
-                 * 0 orderPickupDate
-                 * 1 restaurant
-                 * 2 orderByDate     // order coordinator only
-                 * 3 numberOfOrders  // order coordinator only
-                 * 4 userID
-                 * 5 orderText
-                 * 6 orderPrice
-                 */
-                if (curVal[0] > curDate) {
-                    if (obj[curVal[1]] === undefined) {
-                        obj[curVal[1]] = {};
-                        // first order will be from order admin
-                        obj[curVal[1]].pickupDate = curVal[0];
-                        obj[curVal[1]].orderByDate = curVal[2];
-                        obj[curVal[1]].numOfOrders = Number(curVal[3]);
-                        obj[curVal[1]].orders = [];
+            {}
+        );
+    }());
+    const getJson = function () {
+        return json;
+    };
+    return {
+        getJson
+    };
+}());
+
+/**
+* Main
+* @param {object} $ jQuery reference
+*/
+const Main = (function ($) {
+    "use strict";
+    const tableHtml = $("table").html();
+    /**
+    */
+    const _addTableHtml = function (num) {
+        $("div").append(
+            `
+            <table id="table${num}" class="table table-bordered table-striped
+            table-condensed"></table>
+            `
+        );
+        $("table#table" + num).html(tableHtml);
+    };
+    /**
+    */
+    const display = function (ordObj) {
+        Object.keys(ordObj).forEach(
+            function (groupOrdKey, index) {
+                let num = index;
+                let grandTotal = 0;
+                let orderByTime = ordObj[groupOrdKey].orderByTime;
+                _addTableHtml(num);
+                $("table#table" + num + " caption").text(
+                    ordObj[groupOrdKey].restaurant +
+                    " pickup time: " +
+                    Utils.datify(ordObj[groupOrdKey].pickupTime)
+                );
+                ordObj[groupOrdKey].orders.forEach(
+                    function (curVal, index) {
+                        let price = Number(curVal.price);
+                        let tax = price * 0.07;
+                        let total = price + tax;
+                        $("table#table" + num + " tbody").append(
+                            "<tr>" +
+                            "<td class=\"text-center\">" +
+                            (index + 1) + "</td>" +
+                            "<td>" + curVal.userID + "</td>" +
+                            "<td>" + curVal.order + "</td>" +
+                            "<td class=\"money-cell\">" +
+                            Utils.monify(price) + "</td>" +
+                            "<td class=\"money-cell\">" +
+                            Utils.monify(tax) + "</td>" +
+                            "<td class=\"money-cell\">" +
+                            Utils.monify(total) + "</td>" +
+                            "</tr>"
+                        );
+                        grandTotal += total;
                     }
-                    obj[curVal[1]].orders.push({
-                        "placer": curVal[4],
+                );
+                $("table#table" + num + " tfoot").html(
+                    `
+                    <tr>
+                        <th class="text-center" colspan="2">
+                            <a href="#" class="btn btn-info btn-xs"
+                                role="button"
+                                onclick="Orders.addUserOrder(
+                                ${"'" + groupOrdKey + "'"});
+                                return false;">
+                                Enter/Edit Order
+                            </a>
+                        <td class="${(Date.now() > orderByTime)
+                        ? "text-danger"
+                        : "text-success"}">
+                            orders by:
+                            ${Utils.datify(orderByTime)}
+                        </td>
+                        <th class="money-cell" colspan="2">Grand total:</th>
+                        <th class="money-cell">${Utils.monify(grandTotal)}</th>
+                    </tr>
+                    `
+                );
+            }
+        );
+    };
+    return {
+        display
+    };
+}(jQuery));
+
+/**
+* Orders
+*/
+const Orders = (function () {
+    "use strict";
+    const formHtml = $("form").html();
+    /**
+    * add to group order
+    */
+    const addUserOrder = function (groupOrdKey) {
+        $("div.container").empty();
+        $("div.container").html(
+            "<h2>Hog Trough</h2>" +
+            "<form class=\"form-horizontal\">" +
+            formHtml +
+            "</form>" +
+            "<p>" + groupOrdKey + "</p>"
+        );
+        Object.keys(Restaurant.getJson()).forEach(
+            function (key) {
+                $("select").append(
+                    `<option value="${key}">${key}</option>`
+                );
+            }
+        );
+    };
+    /**
+    * edit user order
+    */
+    const editUserOrder = function () {
+        return;
+    };
+    const get = function () {
+        return ORDERS_ARR.reduce(
+            /**
+            * restaurant (key 1)
+            * filed time (key 2)
+            * order by time in milliseconds
+            * pickup time in milliseconds
+            * userID
+            * order text
+            * price
+            */
+            function (obj, curVal) {
+                let ordKey = curVal[0].replace(/\W/g, "") + curVal[1];
+                if (obj[ordKey] === undefined) {
+                    obj[ordKey] = {};
+                    obj[ordKey].restaurant = curVal[0];
+                    obj[ordKey].orderByTime = curVal[2];
+                    obj[ordKey].pickupTime = curVal[3];
+                    obj[ordKey].orders = [];
+                }
+                obj[ordKey].orders.push(
+                    {
+                        "userID": curVal[4],
                         "order": curVal[5],
                         "price": curVal[6]
-                    });
-                }
-                return obj;
-            }, {});
-    };
-    let formatOrder = function (ord, tableID) {
-        let price = Number(ord.price);
-        let tax = Number(price) * 0.07;
-        let total = price + tax;
-        $("table#" + tableID + " tbody").append(
-            "<tr>" +
-            "<td>" +
-            ord.placer +
-            "</td>" +
-            "<td>" +
-            ord.order +
-            "</td>" +
-            "<td>" +
-            HT.Utils.monify(price) +
-            "</td>" +
-            "<td>" +
-            HT.Utils.monify(tax) +
-            "</td>" +
-            "<td>" +
-            HT.Utils.monify(total) +
-            "</td>" +
-            "</tr>"
-        );
-        return total;
-    };
-    let display = function (ordersArr) {
-        let ordObj = getObj(ordersArr);
-        let grandTotal = 0;
-        let tableID = "";
-        let rowCount = 0;
-        let ordByDate = {};
-        let pickupDate = {};
-        $("#newgrouporder").attr("hidden", true);
-        $("#ordersview").attr("hidden", false);
-        Object.keys(ordObj).forEach(
-            function (curVal, index) {
-                grandTotal = 0;
-                tableID = ("table" + index);
-                $("div#ordersview").append(tableHtml);
-                $("table#default-table").attr("id", tableID);
-                ordByDate = new Date(ordObj[curVal].orderByDate);
-                pickupDate = new Date(ordObj[curVal].pickupDate);
-                $("table#" + tableID + " caption").html(
-                    curVal +
-                    ",&nbsp;orders&nbsp;by&#58;&nbsp;" +
-                    ordByDate.toLocaleString("en-US") +
-                    ",&nbsp;pickup&nbsp;time&#58;&nbsp;" +
-                    pickupDate.toLocaleString("en-US")
-                );
-                ordObj[curVal].orders.forEach(
-                    function (curVal) {
-                        grandTotal += formatOrder(curVal, tableID);
-                    });
-                $("table#" + tableID + " tfoot tr th.grand-total").html(
-                    HT.Utils.monify(grandTotal)
-                );
-                rowCount = ordObj[curVal].orders.length;
-                if (rowCount < ordObj[curVal].numOfOrders) {
-                    $("table#" + tableID + " tfoot tr th:eq(0)").html(
-                        "<a>Add me/edit my order</a>"
-                    );
-                }
-                $("table#" + tableID + " tfoot tr th:eq(0)").on(
-                    "click",
-                    function () {
-                        HT.OneOrder.new(curVal)
                     }
                 );
-            });
+                return obj;
+            },
+            {}
+        );
     };
-    // get table default html from index.html
-    tableHtml = $("div#ordersview").html();
-    // prevents doubling first table displayed
-    $("div#ordersview").empty();
-    HT.Orders = (function ($) {
-        return {
-            /**
-             * Open Orders page.
-             * @function display
-             * @memberof! HT.Orders
-             * @param {object} jQuery The jQuery global.
-             */
-            display
-        };
-    }(jQuery));
+    /**
+    * new group order
+    */
+    const newGroupOrder = function () {
+        return;
+    };
+    return {
+        addUserOrder,
+        editUserOrder,
+        get,
+        newGroupOrder
+    };
 }());
 
-/**
- * [jQuery ready function]{@link https://api.jquery.com/ready/} invocation.
- * @namespace jQuery-ready-fxn
- * @memberof HT
- */
-$(function (ordersArr) {
-    "use strict";
-    /**
-     * Handler for Restaurant select event.
-     * @name change-restaurant
-     * @memberof! HT.jQuery-read-fxn
-     */
-    $("select#restaurant").on(
-        "change",
-        function () {
-            $("a#menulink").attr(
-                "href",
-                HT.Restaurant.get()[
-                    $("select#restaurant option:selected").val()
-                ]
-            );
-        });
-    /**
-     * Handler for New Group Order submit event.
-     * @name submit-new-group-order
-     * @memberof! HT.jQuery-ready-fxn
-     */
-    $("form#newgrouporderform").on(
-        "submit",
-        function (event) {
-            let formObj = $("form").serializeArray()
-                .reduce(
-                    function (formObj, curVal) {
-                        formObj[curVal.name] = curVal.value;
-                        return formObj;
-                    }, {});
-            event.preventDefault();
-            HT.Orders.display(formObj);
-        });
-    /**
-     * Display Orders
-     * @name display-orders
-     * @memberof! HT.jQuery-ready-fxn
-     */
-    HT.Orders.display(ordersArr);
-}(ordersArr));
+$(
+    function () {
+        "use strict";
+        let ordObj = Orders.get();
+        Main.display(ordObj);
+    }
+);
